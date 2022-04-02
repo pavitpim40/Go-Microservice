4566d307194bf1b680a15f22b91751f7bb441b8d
Pavit <pavitpim@gmail.com>
2022-04-02 10:39:54 +0700

    use gin

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
+// const json = `{"name":{"first":"Janet","last":"Prichard"},"age":47}`
 import (
-	"github.com/tidwall/gjson"
-	"github.com/tidwall/sjson"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
-
-const json = `{"name":{"first":"Janet","last":"Prichard"},"age":47}`
-
 func main(){
 
+	// value := gjson.Get(json, "name.last")
+	// print(value.String())
 
+	// value2, _ := sjson.Set(json, "name.last", "Pimchanagul")
+	// println(value2)
 
-	
-	value := gjson.Get(json, "name.last")
-	print(value.String())
-
-	value2, _ := sjson.Set(json, "name.last", "Pimchanagul")
-	println(value2)
+	r := gin.Default();
+	r.GET("/ping", func(c *gin.Context){
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run(":8080")
 }
